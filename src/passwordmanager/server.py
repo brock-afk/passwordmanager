abc01641b2f6233b27d6d8a7deb58044add48c8a
brock-afk <brockbrumley95@gmail.com>
2023-09-05 15:06:50 -0500

    add sanic routes and htmx

--- src/passwordmanager/server.py
+++ src/passwordmanager/server.py
@@ -1,12 +1,26 @@
-from sanic import Sanic, Request, json
+import uuid
+
+from sanic import Sanic, Request
 from passwordmanager.vault import JSONVaultRepository
 
 app = Sanic("passwordmanager")
 
 
 @app.get("/vaults")
+@app.ext.template("vaults.html")
 async def get_vaults(request: Request):
     vault_repository = JSONVaultRepository("/data/vaults")
     vaults = await vault_repository.get_all()
 
-    return json({"vaults": vaults})
+    return {"vaults": vaults}
+
+
+@app.post("/vaults/new")
+@app.ext.template("vaults.html")
+async def create_vault(request: Request):
+    vault_repository = JSONVaultRepository("/data/vaults")
+
+    await vault_repository.create(str(uuid.uuid4()))
+    vaults = await vault_repository.get_all()
+
+    return {"vaults": vaults}
