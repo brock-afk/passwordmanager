163b8f18068ed54bbb3d99e36588ce493e28280b
brock-afk <brockbrumley95@gmail.com>
2023-09-15 15:11:59 -0500

    use tailwind for styling

--- src/passwordmanager/server/main.py
+++ src/passwordmanager/server/main.py
@@ -1,14 +1,6 @@
-import pathlib
-
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from passwordmanager.server.routers import vaults
 
 app = FastAPI()
 
-app.mount(
-    "/static",
-    StaticFiles(directory=pathlib.Path("./src/passwordmanager/server/static")),
-    name="static",
-)
 app.include_router(vaults.router)
